169ada9c7dec8d36610aff957da71a6188e01f21
İsmet Ekrem Çoşkun <iecoskun@gmail.com>
2026-01-28 17:54:59 +0300

    Checkpoint 4: establish Activity-UI-event fundamentals

--- app/src/main/java/com/isekco/vestia/MainActivity.kt
+++ app/src/main/java/com/isekco/vestia/MainActivity.kt
@@ -2,14 +2,21 @@ package com.isekco.vestia
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import android.widget.TextView
+import android.widget.Button
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        // Şimdilik sadece şunu bilmek yeterli:
-        // - onCreate = Activity'nin doğumu
-        // - Burada setContentView yapılır
+
+        val titleText = findViewById<TextView>(R.id.titleText)
+        val button = findViewById<Button>(R.id.actionButton)
+
+        button.setOnClickListener {
+            titleText.text = "This is really Vestia"
+        }
     }
 }
